bf374076463d4e12c5edc791d16967ccddc11e76
Stephen Courtier <steves0089@gmail.com>
2018-09-16 15:49:14 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -5,8 +5,9 @@ def prime?(n)
     return false 
   elsif n == 3 || n ==2
     return true
-    if (2...n-1).any? do 
-      |i| n % i == 0
+  end
+  if (2...n-1).any? do 
+    |i| n % i == 0
       false
     else
       true
