16d110200630dd9944efd70d97a933bad600f71c
Stephen Courtier <steves0089@gmail.com>
2018-09-16 15:48:16 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,12 +1,14 @@
 # Add  code here!
 
-def prime?(num)
-  for d in 2..(n - 1)
-   if (n % d) == 0
-    return false
-   end
-  end
-
-  true
- end
+def prime?(n)
+  if n < 2 
+    return false 
+  elsif n == 3 || n ==2
+    return true
+    if (2...n-1).any? do 
+      |i| n % i == 0
+      false
+    else
+      true
+    end
 end
